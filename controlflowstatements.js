5037cd434368c507479a4e10ef35d719908aef93
Sugavanan0703 <sivasuga9965@gmail.com>
2024-10-19 12:09:02 +0530

    modified js file

--- controlflowstatements.js
+++ controlflowstatements.js
@@ -1,13 +1,29 @@
-console.log("Hello World!")
-console.log(3+2)
-var a=10
-var b=20
-if(a>b){
-    console.log("A is greater than B")
-}
-else if(a==b){
-    console.log("A is equals to B")
-}
-else{
-    console.log("A is lesser than B")
+// console.log("Hello World!")
+// console.log(3+2)
+//----------------------------------- conditional Statements-------------------
+// var a=10
+// var b=20
+// if(a>b){
+//     console.log("A is greater than B")
+// }
+// else if(a==b){
+//     console.log("A is equals to B")
+// }
+// else{
+//     console.log("A is lesser than B")
+// }
+// ----------------------------------Switch Statements ------------------------
+var color="Black"
+switch(color){
+    case "Pink":
+        console.log("Color is Pink")
+        break
+    case "Red":
+        console.log("Color is Red")
+        break
+    case "Black":
+        console.log("Color is Black")
+        break
+    default:
+        console.log("Color not matching")
 }
