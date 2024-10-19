d154ce6a13e5c53ecdd928ce60f532987500e41a
Sugavanan0703 <sivasuga9965@gmail.com>
2024-10-19 12:03:33 +0530

    modified js file

--- controlflowstatements.js
+++ controlflowstatements.js
@@ -1,2 +1,13 @@
 console.log("Hello World!")
 console.log(3+2)
+var a=10
+var b=20
+if(a>b){
+    console.log("A is greater than B")
+}
+else if(a==b){
+    console.log("A is equals to B")
+}
+else{
+    console.log("A is lesser than B")
+}
