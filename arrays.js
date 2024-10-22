039e89e50b2d09db25b99b42b8dd704389783c36
Sugavanan0703 <sivasuga9965@gmail.com>
2024-10-22 23:47:45 +0530

    Modifed arrays.js file

---
+++ arrays.js
@@ -0,0 +1,115 @@
+//------------------Arrays in JavaScript-----------------
+//ways to create Array in javaScript-------------
+//---------1.tredtional way
+// var arr=[1,2,3,4,5];
+// console.log(arr);
+//---------2.using empty square brakets word
+// var arr=[]
+// arr[0]=1;
+// arr[1]=2;
+// arr[2]=3;
+// arr[3]=4;
+// arr[4]=5;
+// console.log(arr);
+//---------3.using Arrays Constructor
+// var arr=new Array()
+// arr[0]=1;
+// arr[1]=2;
+// arr[2]=3;
+// arr[3]=4;
+// arr[4]=5;
+// console.log(arr);
+//------------Inserting values to the array
+//------------1.using push() method
+//------------It adds the elements at rare end or tail end-------
+// var arr=new Array()
+// arr.push(1)
+// arr.push(2)
+// arr.push(3)
+// arr.push(4)
+// arr.push(5)
+// console.log(arr)
+//------------2.using unshift() method
+//-----------It adds the element at the front end--------
+// var arr=new Array();
+// arr.unshift(1)
+// arr.unshift(2)
+// arr.unshift(3)
+// arr.unshift(4)
+// arr.unshift(5)
+// console.log(arr)
+//--------------3.using index positions
+//--------------It adds the elements at the particular index. 
+//--------------If any elements present at the index it will override that.
+// var arr=new Array()
+// arr[0]=1;
+// arr[1]=2;
+// arr[2]=3;
+// arr[3]=4;
+// arr[4]=5;
+// console.log(arr)
+// arr[0]=5
+// console.log(arr)
+// ----------------4. using splice() method
+//-----------------It adds the elements at the particular index.
+//-----------------If any elements already present in the index it will shift it to the s=next index.
+// var arr=new Array()
+// arr[0]=1;
+// arr[1]=2;
+// arr[2]=3;
+// arr[3]=4;
+// arr[4]=5;
+// console.log(arr)
+// arr.splice(2,0,22)
+// console.log(arr)
+//------------Deleting elements in the Arrays 
+//-----------1.using pop() method 
+//-----------It delets the elements at the rear-end or tail-end.
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+//----------2.using shift() method
+//----------It delets the elements in the front-end.
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// arr.shift()
+// console.log(arr)
+//---------3.using delete() method 
+//---------It delete the elements at the particular index.
+//---------but it will make it as empty space.
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// delete arr[0]
+// console.log(arr)
+//---------4.using splice method()
+//---------It delets the elements at the particular index
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// arr.splice(2,1)
+// console.log(arr)
+//----------------Arrays concatination-----------
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// var arr1=[6,7,8,9,10]
+// console.log(arr1)
+// var arr=arr.concat(arr1)
+// console.log(arr)
+//-----------------Arrays Sorting-------
+// var arr=[10,2,34,21,1]
+// console.log(arr)
+// arr.sort(function(a,b){
+//     return b-a
+// })
+// console.log(arr)
+//-------------Converting arrays to string------
+//-------------1.using toSting() method
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// var str=arr.toString()
+// console.log(str)
+//-------------2.using join() method-------
+var arr=[1,2,3,4,5]
+console.log(arr)
+var str=arr.join("-")
+console.log(str)
