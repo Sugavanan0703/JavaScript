9de399fe7f0ef1211c96c5274dc77f95ab7eebfd
Sugavanan0703 <sivasuga9965@gmail.com>
2024-10-24 00:11:37 +0530

    modified arrays.js file

--- arrays.js
+++ arrays.js
@@ -109,7 +109,62 @@
 // var str=arr.toString()
 // console.log(str)
 //-------------2.using join() method-------
-var arr=[1,2,3,4,5]
-console.log(arr)
-var str=arr.join("-")
-console.log(str)
+// var arr=[1,2,3,4,5]
+// console.log(arr)
+// var str=arr.join("-")
+// console.log(str)
+//--------------reverse method()------------
+// var arr=[1,2,3,4,5]
+// var arr1=arr.reverse()
+// console.log(arr1)
+//-------------slice() method---------
+//-------------It gives sub array of the arrays
+// var arr=[1,2,3,4,5,6,7]
+// console.log(arr)
+// console.log(arr.slice(1,5))
+//-------------at() method----------
+//-------------gives the value at the given index
+// var arr=[1,2,3,4,5,6,7]
+// var num=arr.at(5)
+// console.log(num)
+//---------------Advance methods in arrays in JS-------------------
+//---------------1.indexOf(value) method
+//---------------It gives the index of firstOccerence of the given value
+// var arr=[1,2,3,4,5,6,7]
+// console.log(arr.indexOf(5))
+//--------------2.indexOf(value, startsFrom)
+//--------------It starts from the given index and gives first occurrence of the given value 
+// var arr=[1,2,3,4,5,6,7,5]
+// console.log(arr.indexOf(5,5))
+//--------------3.lastindexOf() method
+//--------------It gives the last index of the given elements
+// var arr=[1,2,3,4,5,6,7,5]
+// console.log(arr.lastIndexOf(5))
+//--------------4.includes() method
+//--------------It checks if the element present or not
+// var arr=[1,2,3,4,5,6,7]
+// console.log(arr.includes(2))
+//--------------5.find() method 
+//-------------It used to check do some operations and then returns only one value
+// var arr=[1,2,3,4,5,6,7]
+// var num=arr.find(greater)
+// console.log(num)
+// function greater(value){
+//     return value>4
+// }
+//-------------6.filter() method
+//-------------It used to perform some operations and returns all the values passes the operation
+// var arr=[1,2,3,4,5,6,7]
+// var num=arr.filter(greater)
+// console.log(num)
+// function greater(value){
+//     return value>4
+// }
+//------------7.map() method
+//------------It used to perform some operation and manipulate the data and returns all velues
+// var arr=[1,2,3,4,5,6,7]
+// var num=arr.map(squar)
+// console.log(num)
+// function squar(value){
+//     return value**2
+// }
